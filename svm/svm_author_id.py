df1e3b575bbcbc5379bba17a0d081e13501c1d10
KHOUTIR Amir <amirkhoutir@gmail.com>
2019-05-19 22:22:16 +0100

    SVM : Testing the optimized model with rbf kernel and C = 10000

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,25 +19,19 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-features_train = features_train[:len(features_train)/100] 
-labels_train = labels_train[:len(labels_train)/100] 
-
-
 #########################################################
 ### your code goes here ###
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
-c_value = 10
+c_value = 10000
 
-for i in range(1,5):
-    clf = SVC(kernel="rbf", C = c_value)
-    clf.fit(features_train,labels_train)
+clf = SVC(kernel="rbf", C = c_value)
+clf.fit(features_train,labels_train)
 
-    pred = clf.predict(features_test)
-    acc = accuracy_score(pred, labels_test)
-    print c_value,":", acc
-    c_value = c_value * 10
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+print c_value,":", acc
 
 #########################################################
 
