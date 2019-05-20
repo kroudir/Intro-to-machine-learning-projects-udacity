e46efc1a56ad1b1a9925f2ee35b98f32a266ad3a
KHOUTIR Amir <amirkhoutir@gmail.com>
2019-05-20 17:48:26 +0100

    DT : Testinng DT with min_sample_split = 40

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,13 +19,15 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
-### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
-
+clf = tree.DecisionTreeClassifier( min_samples_split = 40)
+clf.fit(features_train,labels_train)
+pred = clf.predict(features_test)
+acc =accuracy_score(pred,labels_test)
+print acc
 #########################################################
 
 
