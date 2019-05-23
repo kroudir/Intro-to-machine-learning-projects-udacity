dd0c0eee73465fee26f8b2ec637dafc02c5f1cf7
KHOUTIR Amir <amirkhoutir@gmail.com>
2019-05-23 17:05:22 +0100

    Datasets and questions : Reading the dataser

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -17,6 +17,6 @@
 
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-
+print len(enron_data)
