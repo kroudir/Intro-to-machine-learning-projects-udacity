3921104b6521d55855d4930d311daafa72e4581a
KHOUTIR Amir <amirkhoutir@gmail.com>
2019-05-24 16:13:59 +0100

    Regression : Done the model

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r "
 
 
 
@@ -37,8 +37,11 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
-
-
+from sklearn.linear_model import LinearRegression
+mod = LinearRegression()
+mod.fit(feature_train,target_train)
+print "Slope ", mod.coef_
+print "Intercept", mod.intercept_
 
 
 
