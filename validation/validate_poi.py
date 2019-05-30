01a4ca6acac14f79b0537b95f05732e1340bfce3
KHOUTIR Amir <amirkhoutir@gmail.com>
2019-05-30 10:57:52 +0100

    Validation: Desicion tree with cross validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -28,9 +28,13 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn.model_selection import train_test_split
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
+
 from sklearn.tree import DecisionTreeClassifier
 
 clf = DecisionTreeClassifier()
-clf.fit(features, labels)
-print "Accuracy score:", clf.score(features,labels)
+clf.fit(features_train, labels_train)
+print "Accuracy score:", clf.score(features_test,labels_test)
 
