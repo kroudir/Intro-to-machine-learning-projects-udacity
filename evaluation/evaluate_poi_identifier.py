bbab24347827f8f72de9decc34220acf707b24c4
KHOUTIR Amir <amirkhoutir@gmail.com>
2019-06-02 09:47:46 +0100

    Evaluation : Done

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -27,5 +27,43 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+from sklearn.model_selection import train_test_split
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
+
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print "Accuracy score:", clf.score(features_test,labels_test)
+
+preds = clf.predict(features_test)
+
+num_pois = 0
+
+for pred in preds:
+	if pred == 1:
+		num_pois += 1
+
+print "Number of POIs predicted:", num_pois
+print "Total number of people in test set:", len(preds)
+
+true_positives = 0
+false_positives = 0
+false_negatives = 0
+
+for pred, actual in zip(preds, labels_test):
+	if pred == actual and actual == 1:
+		true_positives += 1
+	elif pred == 1 and actual == 0:
+		false_positives += 1
+	elif pred == 0 and actual == 1:
+		false_negatives += 1
+
+print "True Positives:", true_positives
+print "False Positives", false_positives
+print "False Negatives", false_negatives
+
+print "\nPrecision (POIs):", true_positives/(true_positives + false_positives)
+print "Recall (POIs)", true_positives/(true_positives + false_negatives)
 
